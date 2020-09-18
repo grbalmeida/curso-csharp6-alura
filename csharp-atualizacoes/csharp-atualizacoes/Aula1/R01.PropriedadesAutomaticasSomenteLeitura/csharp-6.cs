940b3349a50fcd9d36d1c02f67623990fe0b8955
gilvan <g.r.almeida@live.com>
2020-09-18 10:47:37 -0300

    Propriedades Automáticas

--- csharp-atualizacoes/csharp-atualizacoes/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
+++ csharp-atualizacoes/csharp-atualizacoes/Aula1/R01.PropriedadesAutomaticasSomenteLeitura/csharp-6.cs
@@ -7,6 +7,25 @@ namespace CSharp6.R01
         public void Main()
         {
             Console.WriteLine("1. Propriedades Automáticas Somente-Leitura");
+
+            Aluno aluno = new Aluno("Marty", "McFly", new DateTime(1968, 6, 12));
+
+            Console.WriteLine(aluno.Nome);
+            Console.WriteLine(aluno.Sobrenome);
+        }
+    }
+
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataNascimento { get; set; }
+
+        public Aluno(string nome, string sobrenome, DateTime dataNascimento)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataNascimento = dataNascimento;
         }
     }
 }
