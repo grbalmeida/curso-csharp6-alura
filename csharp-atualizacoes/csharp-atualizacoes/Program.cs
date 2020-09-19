e261e250c23e7fc26bec46e339d6b6e34c966619
gilvan <g.r.almeida@live.com>
2020-09-19 19:40:18 -0300

    Monitorando Alteração em Propriedades

--- csharp-atualizacoes/csharp-atualizacoes/Program.cs
+++ csharp-atualizacoes/csharp-atualizacoes/Program.cs
@@ -12,7 +12,8 @@ namespace csharp_atualizacoes
                 "3. Membros Com Corpo De Expressão",
                 "4. Using Static",
                 "5. Operadores Null-Condicionais",
-                "6. Interpolação De Cadeia De Caracteres"
+                "6. Interpolação De Cadeia De Caracteres",
+                "7. Expressões nameOf"
             };
 
             Console.WriteLine("ÍNDICE DE PROGRAMAS");
@@ -52,6 +53,9 @@ namespace csharp_atualizacoes
                         break;
                     case 6:
                         new CSharp6.R06.Programa().Main();
+                        break;
+                    case 7:
+                        new CSharp6.R07.Programa().Main();
                         break;
                     default:
                         break;
